3d932caf23c5bf75ccadba37bb58dc7f1649deb5
MaggieChafee <maggie.chafee@gmail.com>
2023-11-21 20:56:16 -0600

    ability to create a piece of music completed

--- components/forms/musicForm.js
+++ components/forms/musicForm.js
@@ -1,22 +1,65 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { useAuth } from '../../utils/context/authContext';
+import { createMusic, updateMusic } from '../../api/musicData';
 
-function MusicForm() {
+const initialState = {
+  name: '',
+  composer: '',
+  category: '',
+  musicCompleted: false,
+  startDate: '',
+  recording: '',
+};
+
+function MusicForm({ musicObj }) {
+  const [musicFormInput, setMusicFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMusicFormInput((prevState) => (
+      {
+        ...prevState,
+        [name]: value,
+      }
+    ));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const payload = { ...musicFormInput, uid: user.uid };
+    createMusic(payload).then(({ name }) => {
+      const patchPayload = { firebaseKey: name };
+      updateMusic(patchPayload).then(() => {
+        router.push('/');
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (musicObj.firebaseKey) setMusicFormInput(musicObj);
+  }, [musicObj]);
+
   return (
     <>
       <div>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Name of Piece</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control type="text" name="name" value={musicFormInput.name} onChange={handleChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Composer</Form.Label>
-            <Form.Control type="text" placeholder="Password" />
+            <Form.Control type="text" name="composer" value={musicFormInput.composer} onChange={handleChange} />
           </Form.Group>
           <Form.Group>
             <Form.Label>Category</Form.Label>
-            <Form.Select>
+            <Form.Select name="category" value={musicFormInput.category} onChange={handleChange}>
               <option>Select a Category</option>
               <option>Warm-Up</option>
               <option>Scale</option>
@@ -28,22 +71,38 @@ function MusicForm() {
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Reference Recording</Form.Label>
-            <Form.Control type="text" placeholder="Password" />
+            <Form.Control type="text" name="recording" value={musicFormInput.recording} onChange={handleChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Start Date - Change to Date Picker</Form.Label>
-            <Form.Control type="text" placeholder="Password" />
+            <Form.Control type="text" name="startDate" value={musicFormInput.startDate} onChange={handleChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="I've completed this piece!" />
+            <Form.Check type="checkbox" label="I've completed the piece!" name="musicCompleted" value={musicFormInput.musicCompleted} onChange={handleChange} />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            {musicObj.firebaseKey ? 'Update' : 'Create'} Music
           </Button>
         </Form>
       </div>
     </>
   );
 }
+
+MusicForm.propTypes = {
+  musicObj: PropTypes.shape({
+    name: PropTypes.string,
+    composer: PropTypes.string,
+    category: PropTypes.string,
+    startDate: PropTypes.string,
+    musicCompleted: PropTypes.bool,
+    recording: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+MusicForm.defaultProps = {
+  musicObj: initialState,
+};
 
 export default MusicForm;
